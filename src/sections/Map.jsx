e153a57c228f259311d9c74702e3d835c587118d
Hassanvir Singh <hassanvirsingh5@gmail.com>
2026-03-18 15:21:46 -0600

    Update Map.jsx

--- src/sections/Map.jsx
+++ src/sections/Map.jsx
@@ -2,6 +2,17 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 
+import L from 'leaflet';
+
+// Fix Leaflet default icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
 //52.212932316408654, -113.81979731028562
 
 
